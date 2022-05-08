b1a1b17f60fb78e1ba44fbd03702e9f211a2667e
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-05-08 14:58:30 +0500

    DART SERIES IN CLASS

--- function.dart
+++ function.dart
@@ -1,11 +1,18 @@
-void main(List<String> args) {
-  printname();
-  printname();
-  printname();
+void main() {
+  // int p = multiply(12, 32);
+  // print(p);
+  printtable(2);
+
+  printtable(3);
 }
 
-void printname() {
-  print("Hello flutter !");
-  print("hello haseeb ");
-  print("=================");
+int multiply(a, b) {
+  return a * b;
+}
+
+printtable(table) {
+  for (var i = 1; i <= 10; i++) {
+    print("$table * $i = ${table * i}");
+  }
+  print("\n\n\n");
 }
--- main.dart
+++ main.dart
@@ -1,21 +1,38 @@
+// TEST # 1
+// import 'dart:svg';
+// import 'dart:io';
+
 void main() {
-//   abc(name: "abdul", sec: "D");
-//   abc();
-//   abc();
-//   abc();
-  printtable(3);
-  printtable(5);
-}
-
-// abc({name = "Haseeb", phone = "8908439018", sec = "A"}) {
-//   print("Student name is : $name");
-//   print("Student phone number is : $phone");
-//   print("Student Section is : $sec");
-// }
-
-printtable(table) {
-  for (var i = 1; i <= 10; i++) {
-    print("$table X $i = ${table * i}");
+  print("hello world");
+  for (var i = 1; i < 5; i++) {
+    for (var j = 0; j < i; j++) {
+      print("$i \n");
+    }
   }
-  print("================");
+  print("\n");
+  List arr = [1, 5, 9, 13, 17, 21, 25, 29, 33, 37]; // sum = 190
+  num st = 0;
+  // var i;
+  for (var i = 1; i < arr.length; i++) {
+    st = st + arr[i];
+  }
+  print(st);
+
+  var abc = "yes";
+  while (abc == "yes") {
+    print("Enter your emial");
+    var email = stdin.readLineSync();
+    print("Enter your password");
+    var password = stdin.readLineSync();
+    if (email == "abdulhaseeb@gmail.com" && password == "1234") {
+      print("Login successfully");
+      abc = "no";
+    } else {
+      print("Login failed");
+      print("CAN YOU TRY AGAIN");
+      print(" Yes");
+      abc = stdin.readLineSync()!;
+    }
+  }
+
 }
